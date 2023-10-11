42235b9eabbcd8701d64b64fee7b1c1fcd0ddfa8
Jimmy Johnson <johnson.1984@wright.edu>
2023-10-10 23:53:47 -0400

    creation problem

--- Project03/Main.cpp
+++ Project03/Main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <exception>
+#include "sequence.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "hello" << endl;
+    cout << "start" << endl;
+    Sequence test0(0);
+    Sequence test1(1);
+    Sequence test5(5);
+
+    cout << "end" << endl;
 }
--- Project03/sequence.cpp
+++ Project03/sequence.cpp
@@ -2,30 +2,54 @@
 
 Sequence::Sequence(size_type sz)
 {
+    head = nullptr;
+    tail = nullptr;
+    numElts = 0;
+    for (int i = 0; i < sz; i++)
+    {
+        SequenceNode *pointNode = new SequenceNode;
+        SequenceNode newNode = SequenceNode();
+        numElts++;
 
+        if (i == 0)
+        {
+            head = pointNode;
+            newNode.elt = i;
+            newNode.prev = nullptr;
+        }
+        if (i > 0)
+        {
+            newNode.next = nullptr;
+            newNode.prev = sequence[i-1];
+        }
+        if (i == sz - 1)
+        {
+            tail = pointNode;
+            newNode.elt = i;
+            newNode.next = nullptr;
+        }
+    }
 }
 
-Sequence::Sequence(const Sequence& s)
+Sequence::Sequence(const Sequence &s)
 {
-
 }
 
 Sequence::~Sequence()
 {
-
 }
 
-Sequence& Sequence::operator=(const Sequence& s)
+Sequence &Sequence::operator=(const Sequence &s)
 {
     return *this;
 }
 
-Sequence::value_type& Sequence::operator[](size_type position)
+Sequence::value_type &Sequence::operator[](size_type position)
 {
     throw exception();
 }
 
-void Sequence::push_back(const value_type& value)
+void Sequence::push_back(const value_type &value)
 {
 }
 
@@ -39,12 +63,12 @@ void Sequence::insert(size_type position, value_type value)
     throw exception();
 }
 
-const Sequence::value_type& Sequence::front() const
+const Sequence::value_type &Sequence::front() const
 {
     throw exception();
 }
 
-const Sequence::value_type& Sequence::back() const
+const Sequence::value_type &Sequence::back() const
 {
     throw exception();
 }
@@ -61,7 +85,6 @@ Sequence::size_type Sequence::size() const
 
 void Sequence::clear()
 {
-
 }
 
 void Sequence::erase(size_type position, size_type count)
@@ -69,16 +92,15 @@ void Sequence::erase(size_type position, size_type count)
     throw exception();
 }
 
-// Place code for printing sequence here (well not here, inside the method)
-void Sequence::print(ostream& os) const
+// Place code for printing sequence here
+void Sequence::print(ostream &os) const
 {
-
+    os << "printing" << endl;
 }
 
 // Don't modify, do the output in the print() method
-ostream& operator<<(ostream& os, const Sequence& s)
+ostream &operator<<(ostream &os, const Sequence &s)
 {
     s.print(os);
     return os;
 }
-
