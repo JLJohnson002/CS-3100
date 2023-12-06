9251657bf8710a8b49e6068e20578f905daefc4b
Jimmy Johnson <89602279+JLJohnson002@users.noreply.github.com>
2023-12-06 17:19:56 -0500

    all files created

--- Project06/hashfunction.cpp
+++ Project06/hashfunction.cpp
@@ -5,10 +5,10 @@
 
 unsigned int jsHash(const int key)
 {
-   const char* kptr = (char *)&key;
+   const char *kptr = (char *)&key;
    unsigned int length = sizeof(int);
    unsigned int hash = 1315423911;
-   unsigned int i    = 0;
+   unsigned int i = 0;
 
    for (i = 0; i < length; ++kptr, ++i)
    {
